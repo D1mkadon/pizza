6bd28c4af1eb42ef6ff5ae24344d1994b34edbc9
Dmytro <dima0938157970@gmail.com>
2022-07-27 19:21:33 +0300

    pizza

---
+++ src/index.js
@@ -0,0 +1,8 @@
+// Test import of a JavaScript module
+import { example } from '@/js/example'
+// est import of an asset
+import webpackLogo from '@/images/webpack-logo.svg'
+// Test import of styles
+import '@/styles/index.scss'
+// Appending to the DOM
+
